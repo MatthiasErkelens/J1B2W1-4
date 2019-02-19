8456106c0f34a92ba436ac24b9174d34c3a5ff2d
MatthiasErkelens <matthiaserkelens@gmail.com>
2019-02-19 09:48:42 +0100

    adventure game

---
+++ Adventure_Game.js
@@ -0,0 +1,168 @@
+var knop1 = document.getElementById('button1');
+var knop2 = document.getElementById('button2');
+var knop3 = document.getElementById('button3');
+var titel = document.getElementById('title');
+var text = document.getElementById('description');
+var item = document.getElementById('inventoryItem');
+
+function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
+
+    function chestfalseb(){
+console.log('ted');
+    title.innerHTML ='';
+    document.getElementById('game-container').style.backgroundImage = "url('img/chestwithted.png')";
+    document.body.style.backgroundSize='cover';
+    knop1.style.display="none";
+        knop1.innerHTML ='go to the next door!';
+    knop2.style.display="none";
+        knop2.innerHTML ='look over the balcony';
+    knop3.style.display="block";
+        knop3.innerHTML ='go back!';
+
+        knop3.onclick = function(){
+        lvl6();
+    }
